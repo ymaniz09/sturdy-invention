9b04cfa3c831224b68f10fa5a90f3c616b9f6187
Ygor <ygor.santos@thoughtworks.com>
2020-03-05 17:34:38 -0300

    Creating NoteEntity

---
+++ app/src/main/java/com/github/ymaniz09/mymemorynotes/framework/db/NoteEntity.kt
@@ -0,0 +1,24 @@
+package com.github.ymaniz09.mymemorynotes.framework.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.github.ymaniz09.core.data.Note
+
+@Entity(tableName = "note")
+data class NoteEntity(
+    var title: String,
+    var content: String,
+    @ColumnInfo(name = "creation_date")
+    var creationTime: Long,
+    @ColumnInfo(name = "update_date")
+    var updateTime: Long,
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
+) {
+    companion object {
+        fun fromNote(note: Note) = NoteEntity(note.title, note.content, note.creationTime, note.updateTime)
+    }
+
+    fun toNote() = Note(title, content, creationTime, updateTime, id)
+}
