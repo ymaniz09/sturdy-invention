c969c637240514c222250eb13abbf1ea15238f12
Ygor <ygor.santos@thoughtworks.com>
2020-03-05 16:10:54 -0300

    Setting up navigation

--- app/src/main/java/com/github/ymaniz09/mymemorynotes/presentation/NoteFragment.kt
+++ app/src/main/java/com/github/ymaniz09/mymemorynotes/presentation/NoteFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.github.ymaniz09.mymemorynotes.R
+import kotlinx.android.synthetic.main.fragment_note.*
 
 class NoteFragment : Fragment() {
 
@@ -14,8 +16,13 @@ class NoteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        saveNoteFloatingActionButton.setOnClickListener { Navigation.findNavController(it).popBackStack() }
     }
 
 }
